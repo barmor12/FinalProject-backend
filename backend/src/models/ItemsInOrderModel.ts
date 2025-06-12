d76758665ad7c754c3294f58d4c7fd22c204e7db
barmor12 <barmo2@ac.sce.ac.il>
2025-06-12 03:40:25 +0300

    check

--- backend/src/models/ItemsInOrderModel.ts
+++ backend/src/models/ItemsInOrderModel.ts
@@ -1,9 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const orderItemSchema = new mongoose.Schema({
-  order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true }, // קשר להזמנה
-  cake: { type: mongoose.Schema.Types.ObjectId, ref: 'Cake', required: true }, // קשר לעוגה
+  order: { type: mongoose.Schema.Types.ObjectId, ref: "Order", required: true }, // קשר להזמנה
+  cake: { type: mongoose.Schema.Types.ObjectId, ref: "Cake", required: true }, // קשר לעוגה
   quantity: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('OrderItem', orderItemSchema);
+module.exports = mongoose.model("OrderItem", orderItemSchema);
