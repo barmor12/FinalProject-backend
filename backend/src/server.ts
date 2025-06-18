a7d02602b1038e26bef9e69d5645f9acc9f3a69d
barmor12 <barmo2@ac.sce.ac.il>
2025-06-18 21:35:24 +0300

    add privacy policy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,6 +34,9 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+app.get('/privacy', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/privacy.html'));
+});
 // Middleware
 app.use(cors());
 app.use(express.json());
