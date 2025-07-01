ab92a77485f0ee6650d8de2fa5488d828dc833ec
barmor12 <barmo2@ac.sce.ac.il>
2025-07-01 03:29:50 +0300

    test ci cd

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -9,6 +9,7 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
+    
     new winston.transports.Console(),
     new winston.transports.File({ filename: 'logs/app.log' }),
   ],
